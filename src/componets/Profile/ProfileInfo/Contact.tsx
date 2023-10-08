17dbeb6e2c9fc6f0343a749cc8887432d7c690ff
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2023-10-08 19:47:54 +0300

    Commit add styles to the Profile-data

--- src/componets/Profile/ProfileInfo/Contact.tsx
+++ src/componets/Profile/ProfileInfo/Contact.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from '../Profile.module.css';
-import {ContactsType} from '../ProfileAPIClassComponent';
+
 
 type ContactPropsType = {
     contactTitle: string
